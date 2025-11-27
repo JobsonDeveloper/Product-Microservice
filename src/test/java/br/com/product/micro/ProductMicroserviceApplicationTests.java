9485751dd21a6a9f082e0938d16ddf48f1dae5ad
Jobson de Oliveira Coutinho <jobsondeveloper@gmail.com>
2025-11-27 20:12:25 -0300

    Add the project template

---
+++ src/test/java/br/com/product/micro/ProductMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.product.micro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
