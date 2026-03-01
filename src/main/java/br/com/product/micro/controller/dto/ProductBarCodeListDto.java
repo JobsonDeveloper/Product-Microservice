0facb0fe0434b9febc39fdc2858a899f0ece64da
Jobson de Oliveira Coutinho <jobsondeveloper@gmail.com>
2026-03-01 20:27:51 -0300

    Feature: Adds a route to return a list of products data

---
+++ src/main/java/br/com/product/micro/controller/dto/ProductBarCodeListDto.java
@@ -0,0 +1,12 @@
+package br.com.product.micro.controller.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Positive;
+
+import java.util.List;
+
+public record ProductBarCodeListDto(
+        @NotEmpty(message = "The barcode list must not be empty!")
+        List<@Positive(message = "Barcode must be a positive number") Long> products
+) {
+}
