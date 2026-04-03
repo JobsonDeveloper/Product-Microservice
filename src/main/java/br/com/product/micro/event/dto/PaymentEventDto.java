f018757faa54caf7e9197a4e16c00872d9e75fc0
Jobson de Oliveira Coutinho <jobsondeveloper@gmail.com>
2026-04-02 21:34:27 -0300

    Feature: Add json ignore annotation in the payment event dto

--- src/main/java/br/com/product/micro/event/dto/PaymentEventDto.java
+++ src/main/java/br/com/product/micro/event/dto/PaymentEventDto.java
@@ -1,7 +1,9 @@
 package br.com.product.micro.event.dto;
 
 import br.com.product.micro.domain.Status;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public record PaymentEventDto(
         String saleId,
         Status status
