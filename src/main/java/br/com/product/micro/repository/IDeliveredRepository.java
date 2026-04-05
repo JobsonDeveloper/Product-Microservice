a757e574865fa2c1a1239a35a97d96c012a74b52
Jobson de Oliveira Coutinho <jobsondeveloper@gmail.com>
2026-04-05 14:47:22 -0300

    Refactor: Remove the 'delivered' database tale

--- src/main/java/br/com/product/micro/repository/IDeliveredRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.product.micro.repository;
-
-import br.com.product.micro.domain.Delivered;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IDeliveredRepository extends MongoRepository<Delivered, String> {
-}
