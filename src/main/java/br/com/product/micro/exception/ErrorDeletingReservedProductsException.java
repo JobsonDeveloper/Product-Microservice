92f2b4029507daba3c9de55d2b2b10f8cc8f6dc0
Jobson de Oliveira Coutinho <jobsondeveloper@gmail.com>
2026-04-17 16:13:45 -0300

    Refactor: Remove unnecessary exceptions

--- src/main/java/br/com/product/micro/exception/ErrorDeletingReservedProductsException.java
+++
@@ -1,8 +0,0 @@
-package br.com.product.micro.exception;
-
-public class ErrorDeletingReservedProductsException extends RuntimeException {
-    public ErrorDeletingReservedProductsException() {super("It was not possible to delete reserved products!");}
-    public ErrorDeletingReservedProductsException(String message) {
-        super(message);
-    }
-}
